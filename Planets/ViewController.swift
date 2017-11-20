f62282635db0f26854259abd31b777e5ab73e690
Joe Hill <joe@jkh3.com>
2017-11-19 21:30:20 -0500

    Added a planet function, so I don't have to repeat myself, and added in Venus surface images to the Assets. Adding the other planets along with rotation next

--- Planets/ViewController.swift
+++ Planets/ViewController.swift
@@ -23,19 +23,42 @@ class ViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        let earth = SCNNode()
-        earth.geometry = SCNSphere(radius: 0.2)
-        earth.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Earth day")
-        earth.geometry?.firstMaterial?.specular.contents = #imageLiteral(resourceName: "Earth Specular")
-        earth.geometry?.firstMaterial?.emission.contents = #imageLiteral(resourceName: "Earth Emmission")
-        earth.geometry?.firstMaterial?.normal.contents = #imageLiteral(resourceName: "Earth Normal")
-        earth.position = SCNVector3(0,0,-1)
-        self.sceneView.scene.rootNode.addChildNode(earth)
+        let sun = SCNNode(geometry: SCNSphere(radius: 0.35))
+        sun.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Sun Diffuse")
+        sun.position = SCNVector3(0,0,-1)
+        self.sceneView.scene.rootNode.addChildNode(sun)
         
-        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
+        let earth = planet(geometry: SCNSphere(radius: 0.2), diffuse: #imageLiteral(resourceName: "Earth day"), specular: #imageLiteral(resourceName: "Earth Specular"), emission: #imageLiteral(resourceName: "Earth Emmission"), normal: #imageLiteral(resourceName: "Earth Normal"), position: SCNVector3(1.2, 0, 0))
         
-        let forever = SCNAction.repeatForever(action)
-        earth.runAction(forever)
+        let venus = planet(geometry: SCNSphere(radius: 0.1 ), diffuse: #imageLiteral(resourceName: "Venus Surface"), specular: nil, emission: #imageLiteral(resourceName: "Venus Atmosphere"), normal: nil, position: SCNVector3(0.7,0,0))
+        sun.addChildNode(earth)
+        sun.addChildNode(venus)
+//        earth.geometry = SCNSphere(radius: 0.2)
+//        earth.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Earth day")
+//        earth.geometry?.firstMaterial?.specular.contents = #imageLiteral(resourceName: "Earth Specular")
+//        earth.geometry?.firstMaterial?.emission.contents = #imageLiteral(resourceName: "Earth Emmission")
+//        earth.geometry?.firstMaterial?.normal.contents = #imageLiteral(resourceName: "Earth Normal")
+//        earth.position = SCNVector3(1.2,0,0)
+//        sun.addChildNode(earth)
+        
+//        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
+//
+//        let forever = SCNAction.repeatForever(action)
+//        earth.runAction(forever)
+    }
+    
+    func planet(geometry: SCNGeometry, diffuse: UIImage, specular: UIImage?, emission: UIImage?,
+                normal: UIImage?, position: SCNVector3) -> SCNNode {
+        let planet = SCNNode(geometry: geometry)
+        planet.geometry?.firstMaterial?.diffuse.contents = diffuse
+        planet.geometry?.firstMaterial?.specular.contents = specular
+        planet.geometry?.firstMaterial?.emission.contents = emission
+        planet.geometry?.firstMaterial?.normal.contents = normal
+        planet.position = position
+        return planet
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
